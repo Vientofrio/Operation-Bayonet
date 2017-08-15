e8d75d85084013389ed19333faca31231000bbbf
Pedro DeJesus <pjdejesus@fullsail.edu>
2017-08-15 14:11:57 -0400

    Made player character and plane

---
+++ Operation Bayonet/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 6f;              // Player speed
+
+    Vector3 movement;
+    Rigidbody playerRigidbody;
+
+	// Use this for initialization
+	void Start () {
+
+        playerRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Move(x, z);
+        
+	}
+
+    void Move(float x, float z)
+    {
+        movement.Set(x, 0f, z);
+
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+}
